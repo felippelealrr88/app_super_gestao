807a9f40aaa4fbebf7183f0b78d471d87562d130
Felippe <felippelealrr88@hotmail.com>
2024-01-12 13:47:50 -0400

    @empty
    para saber se está vazio

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -9,7 +9,7 @@ class FornecedoresController extends Controller
     public function index(){
         $fornecedores = [
             0 =>['nome'=>'Casa do Pão de Queijo', 'status'=>'ativo', 'cnpj'=>'61.851.929/0001-19'],
-            1 =>['nome'=>'Casa do Camarão',  'cnpj'=>'22.441.389/0001-43']
+            1 =>['nome'=>'Casa do Camarão', 'status'=>'ativo', 'cnpj'=>'']
         ];
                 return view('app.fornecedores.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -1,5 +1,18 @@
 <h3>Fornecedores</h3>
 
+@php
+    /*if(empty(@variavel)){} retorna true se a variável estiver vazia
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var
+    */
+@endphp
+
 @isset($fornecedores[0]['nome'])
     Fornecedor 1: {{$fornecedores[0]['nome']}}<br/>    
 @endisset
@@ -11,11 +24,14 @@
 @endisset        
 
 @isset($fornecedores[1]['nome'])
-    Fornecedor 1: {{$fornecedores[0]['nome']}}<br/>    
+    Fornecedor 1: {{$fornecedores[1]['nome']}}<br/>    
 @endisset
 @isset($fornecedores[1]['status'])
-    Status: {{$fornecedores[0]['status']}}<br/>
+    Status: {{$fornecedores[1]['status']}}<br/>
 @endisset
 @isset($fornecedores[1]['cnpj'])
-    Cnpj: {{$fornecedores[0]['cnpj']}}<br/><br/> 
+    Cnpj: {{$fornecedores[1]['cnpj']}}
+    @empty($fornecedores[1]['cnpj'])
+        - Vazio
+    @endempty
 @endisset    
