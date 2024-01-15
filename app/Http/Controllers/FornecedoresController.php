8093459e32384781254d11b2f8f16bacca55bf5c
Felippe <felippelealrr88@hotmail.com>
2024-01-15 11:59:43 -0400

    Switch case em BladePHP

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -8,10 +8,25 @@ class FornecedoresController extends Controller
 {
     public function index(){
         $fornecedores = [
-            0 =>['nome'=>'Casa do Pão de Queijo', 'status'=>'ativo', 'cnpj'=>'61.851.929/0001-19'],
-            1 =>['nome'=>'Casa do Camarão', 'status'=>'ativo']
+            0 =>['nome'=>'Casa do Pão de Queijo',
+            'status'=>'ativo',
+            'cnpj'=>'61.851.929/0001-19',
+            'ddd'=>'11',
+            'telefone'=>'99150-2221'],
+
+            1 =>['nome'=>'Casa do Camarão',
+            'status'=>'ativo',
+            'cnpj'=>'22.441.389/0001-43',
+            'ddd'=>'95',
+            'telefone'=>'3623-5294'],
+
+            2 =>['nome'=>'Massas e Panquecas',
+            'status'=>'ativo',
+            'cnpj'=>' 09.087.447/0002-03',
+            'ddd'=>'98',
+            'telefone'=>'3224-6969']
         ];
 
-                return view('app.fornecedores.index', compact('fornecedores'));
+            return view('app.fornecedores.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -13,19 +13,82 @@
     */
 @endphp
 
-@isset($fornecedores[0]['nome'])
-    Fornecedor 1: {{$fornecedores[0]['nome']}}<br/>    
-@endisset
-@isset($fornecedores[1]['status'])
-    Status: {{$fornecedores[1]['status']}}<br/>
-@endisset
+@isset($fornecedores)
+    
+    Fornecedor 1: {{$fornecedores[0]['nome'] ?? 'Sem nome' }}<br/>    
+    Status: {{$fornecedores[0]['status'] ?? 'Indefinido' }}<br/>
+    CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Dados não preenchidos' }}<br/>
+    ddd: {{$fornecedores[0]['ddd'] ?? '00' }}<br/>
+    Telefone: {{$fornecedores[0]['telefone'] ?? '00000-0000' }}<br/>
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+        @break
+        @case('98')
+            São Luiz - MA
+        @break
 
-CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dados não preenchidos' }}
+        @case('95')
+            Boa Vista - RR
+        @break    
+        @default
+        Estado não informado
+            
+    @endswitch <br/><br/>
 
-<!-- VALOR DEFAULT (??)
-Se a variável testada não estiver definida 
-ou
-Se a variável testada possuir o valor null
--->
+
+    Fornecedor 2: {{$fornecedores[1]['nome'] ?? 'Sem nome' }}<br/>    
+    Status: {{$fornecedores[1]['status'] ?? 'Indefinido' }}<br/>
+    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dados não preenchidos' }}<br/>
+    ddd: {{$fornecedores[1]['ddd'] ?? '00' }}<br/>
+    Telefone: {{$fornecedores[1]['telefone'] ?? '00000-0000' }}<br/>
+    @switch($fornecedores[1]['ddd'])
+        @case('11')
+            São Paulo - SP
+        @break
+        @case('98')
+            São Luiz - MA
+        @break
+
+        @case('95')
+            Boa Vista - RR
+        @break    
+        @default
+        Estado não informado
+            
+    @endswitch <br/><br/>
+
+
+    Fornecedor 3: {{$fornecedores[2]['nome'] ?? 'Sem nome' }}<br/>    
+    Status: {{$fornecedores[2]['status'] ?? 'Indefinido' }}<br/>
+    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dados não preenchidos' }}<br/>
+    ddd: {{$fornecedores[2]['ddd'] ?? '00' }}<br/>
+    Telefone: {{$fornecedores[2]['telefone'] ?? '00000-0000' }}<br/>
+    
+    @switch($fornecedores[2]['ddd'])
+        @case('11')
+            São Paulo - SP
+        @break
+        @case('98')
+            São Luiz - MA
+        @break
+
+        @case('95')
+            Boa Vista - RR
+        @break    
+        @default
+            Estado não informado
+            
+    @endswitch
+    
+    <br/><br/>
+
+    <!-- VALOR DEFAULT (??)
+    Se a variável testada não estiver definida 
+    ou
+    Se a variável testada possuir o valor null
+    -->
+    
+@endisset
 
   
