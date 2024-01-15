1395eae1c044d3a02553773823a23b75c933adec
Felippe <felippelealrr88@hotmail.com>
2024-01-15 11:30:34 -0400

    Tratamento de dados com BladePHP
    Estabelecendo valores padrões para variáveis em caso de valores nulos ou não definidos

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -9,12 +9,8 @@ class FornecedoresController extends Controller
     public function index(){
         $fornecedores = [
             0 =>['nome'=>'Casa do Pão de Queijo', 'status'=>'ativo', 'cnpj'=>'61.851.929/0001-19'],
-            1 =>['nome'=>'Casa do Camarão', 'status'=>'ativo', 'cnpj'=>'']
+            1 =>['nome'=>'Casa do Camarão', 'status'=>'ativo']
         ];
-
-//Operador condicional ternário em PHP (condção ? se verdade : se falso;)
-echo isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
-
 
                 return view('app.fornecedores.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -16,22 +16,16 @@
 @isset($fornecedores[0]['nome'])
     Fornecedor 1: {{$fornecedores[0]['nome']}}<br/>    
 @endisset
-@isset($fornecedores[0]['status'])
-    Status: {{$fornecedores[0]['status']}}<br/>
-@endisset
-@isset($fornecedores[0]['cnpj'])
-    Cnpj: {{$fornecedores[0]['cnpj']}}<br/><br/>
-@endisset        
-
-@isset($fornecedores[1]['nome'])
-    Fornecedor 1: {{$fornecedores[1]['nome']}}<br/>    
-@endisset
 @isset($fornecedores[1]['status'])
     Status: {{$fornecedores[1]['status']}}<br/>
 @endisset
-@isset($fornecedores[1]['cnpj'])
-    Cnpj: {{$fornecedores[1]['cnpj']}}
-    @empty($fornecedores[1]['cnpj'])
-        - Vazio
-    @endempty
-@endisset    
+
+CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dados não preenchidos' }}
+
+<!-- VALOR DEFAULT (??)
+Se a variável testada não estiver definida 
+ou
+Se a variável testada possuir o valor null
+-->
+
+  
