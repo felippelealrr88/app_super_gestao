645882806ffd82e0f6236d302ec3b5d38a46e2c1
Felippe <felippelealrr88@hotmail.com>
2024-01-15 11:19:51 -0400

    Operadores condicionais ternários em PHP

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -11,6 +11,11 @@ class FornecedoresController extends Controller
             0 =>['nome'=>'Casa do Pão de Queijo', 'status'=>'ativo', 'cnpj'=>'61.851.929/0001-19'],
             1 =>['nome'=>'Casa do Camarão', 'status'=>'ativo', 'cnpj'=>'']
         ];
+
+//Operador condicional ternário em PHP (condção ? se verdade : se falso;)
+echo isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+
+
                 return view('app.fornecedores.index', compact('fornecedores'));
     }
 }
