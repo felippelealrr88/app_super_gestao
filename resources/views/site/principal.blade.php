b1a23c7e25ef2489e9fb719d292ba93bfd623d61
Felippe <felippelealrr88@hotmail.com>
2024-01-17 11:03:16 -0400

    usando @include para topo e rodapé

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,21 +1,8 @@
 @extends('site.layouts.basico')
 
+    @section('titulo', 'Principal')
     @section('conteudo')
-        <div class="topo">
-
-            <div class="logo">
-                <img src="{{asset('img/logo.png')}}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-
+    
         <div class="conteudo-destaque">
         
             <div class="esquerda">
@@ -62,23 +49,6 @@
                 </div>
             </div>
         </div>
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="{{asset('img/facebook.png')}}">
-                <img src="{{asset('img/linkedin.png')}}">
-                <img src="{{asset('img/youtube.png')}}">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="{{asset('img/mapa.png')}}">
-            </div>
-        </div>
+
     @endsection    
     
