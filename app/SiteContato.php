42a5bc97ac2fc3907138769924bd4da630676801
Felippe <felippelealrr88@hotmail.com>
2024-01-29 11:44:12 -0400

    informações sobre eloquent

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    /*Model serve para fazer o mapeamento Objeto Relacional
+    ou seja, manipular os objetos da aplicação no banco de maneira facilitada
+    
+    Regra Eloquent nome de Classe e tabela no banco
+    1 - SiteContato   ->separação por underline _
+    2 - Site_Contato ->conversão em minúsculas
+    3 - site_contatos ->conversão em plural (s)
+    
+    */
 }
