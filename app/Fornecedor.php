32ad84f2c989a1a8f6a338da73de403fbb9cf17a
Felippe <felippelealrr88@hotmail.com>
2024-02-01 10:21:46 -0400

    adcionando observações

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -11,3 +11,13 @@ class Fornecedor extends Model
     //Permissão para inserir dados no Objeto usando Tinker
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
+
+/*Model serve para fazer o mapeamento Objeto Relacional
+    ou seja, manipular os objetos da aplicação no banco de maneira facilitada
+    
+    Regra Eloquent nome de Classe e tabela no banco
+    1 - SiteContato   ->separação por underline _
+    2 - Site_Contato ->conversão em minúsculas
+    3 - site_contatos ->conversão em plural (s)
+    
+    */
