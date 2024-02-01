943223d622fb82621599263ffc2a83e017964576
Felippe <felippelealrr88@hotmail.com>
2024-02-01 09:40:10 -0400

    Update Fornecedor.php

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //Atributo que sobrepõe a nomeação automática do Laravel
+    protected $table = 'fornecedores';
 }
