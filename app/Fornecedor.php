c3c8b81942a8e323d1c78a9cff6024d6eb6a1380
Felippe <felippelealrr88@hotmail.com>
2024-02-01 10:09:04 -0400

    Update Fornecedor.php

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,6 +8,6 @@ class Fornecedor extends Model
 {
     //Atributo que sobrepõe a nomeação automática do Laravel no banco
     protected $table = 'fornecedores';
-    //Permissão para inserir dados usando Tinker
+    //Permissão para inserir dados no Objeto usando Tinker
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
