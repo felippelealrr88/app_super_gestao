515a5aecdb3e2df3f3ae9da6b0a0856dc97f517c
Felippe <felippelealrr88@hotmail.com>
2024-03-25 12:19:35 -0400

    Usando Middlewares dentro dos Controllers

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
+    public function __construct()
+    {
+        //Adicionando o middlaware diretamente no construtor
+       $this->middleware(LogAcessoMiddleware::class); 
+    }
     public function sobreNos(){
         return view('site.sobre-nos');
     }
