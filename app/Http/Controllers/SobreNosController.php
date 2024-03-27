186046a01cf1a58d2c5b7fd73cf4b0683ffe6823
Felippe <felippelealrr88@hotmail.com>
2024-03-27 10:53:58 -0400

    Retirada do middleware do SobreNosController

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,11 +7,6 @@ use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
 {
-    public function __construct()
-    {
-        //Adicionando o middlaware diretamente no construtor
-       $this->middleware(LogAcessoMiddleware::class); 
-    }
     public function sobreNos(){
         return view('site.sobre-nos');
     }
