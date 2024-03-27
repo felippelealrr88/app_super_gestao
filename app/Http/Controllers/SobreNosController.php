0f07d92cb0e012ed5a9bbe54c76f96d786d5a1e9
Felippe <felippelealrr88@hotmail.com>
2024-03-27 11:00:54 -0400

    usando os middlewares do laravel com App\Http\Kernel

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\LogAcessoMiddleware;
+
 
 class SobreNosController extends Controller
 {
+    
     public function sobreNos(){
         return view('site.sobre-nos');
     }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //Chamada do Middleware que criamos
+            \App\Http\Middleware\LogAcessoMiddleware::class
         ],
 
         'api' => [
