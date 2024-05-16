6d18153a67fc6c7a4e3dbcfa06b656d6bd0a1b8b
Felippe <felippelealrr88@hotmail.com>
2024-05-16 11:54:57 -0400

    Implementando a listagem de clientes

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -2,81 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Cliente;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    
+    public function index(Request $request)
     {
-        return view('app.cliente.index');
+        //Recebe os clientes do banco
+        $clientes = Cliente::paginate(10);
+
+        return view('app.cliente.index', ['clientes' => $clientes, 'request' => $request->all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         //
--- resources/views/app/cliente/index.blade.php
+++ resources/views/app/cliente/index.blade.php
@@ -1,12 +1,58 @@
 @extends('app.layouts.basico')
 
-@section('titulo', 'Cliente')
+@section('titulo', 'Clientes')
 
 @section('conteudo')
-    <br><br><br><br><br><br>
-    <h1>Cliente</h1>
+
+    <div class="conteudo-pagina">
+
+        <div class="titulo-pagina-2">
+            <p>Listagem de Clientes</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('cliente.create')}}">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width: 90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Visualizar</th>
+                            <th>Editar</th>
+                            <th>Excluir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($clientes as $cliente)
+                        <tr>
+                            <td>{{ $cliente->nome }}</td>
+                            
+                            <td><a href=" {{ route('cliente.show', $cliente->id)}} ">Visualizar</a></td>
+                            <td><a href=" {{ route('cliente.edit', $cliente->id)}} ">Editar</a></td>
+                            <td>
+                                <form method="post" action="{{ route('cliente.destroy', $cliente->id) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit">Excluir</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                {{ $clientes->appends($request)->links()}}
+                <br>
+                Exibindo {{$clientes->count()}} produtos de {{$clientes->total()}} (de {{$clientes->firstItem()}} até {{$clientes->lastItem()}})
+            </div>
+        </div>
+
+    </div>
+
 @endsection
-
-
-
-  
